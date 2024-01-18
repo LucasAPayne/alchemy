3a92096ff2664ce361c50d7a895ba17dde08d8d4
Lucas Payne <lucasapayne@outlook.com>
2024-01-17 20:22:17 -0500

    Made including example projects optional

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@
 IF NOT EXIST build mkdir build
 pushd build
 DEL *.pdb >nul 2>&1
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DALCHEMY_INCLUDE_EXAMPLES=ON ..
 cmake --build . -j
 popd
