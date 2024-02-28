7aa2d2c7d68228bcc1529fc79c51ad92ef7a919b
Lucas Payne <lucasapayne@outlook.com>
2024-02-28 13:52:30 -0500

    Fixed UI rendering issue

--- src/renderer/ui.c
+++ src/renderer/ui.c
@@ -94,6 +94,7 @@ void ui_render(Renderer* renderer, enum nk_anti_aliasing aa)
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glDisable(GL_CULL_FACE);
     glDisable(GL_DEPTH_TEST);
+    glDisable(GL_STENCIL_TEST);
     glEnable(GL_SCISSOR_TEST);
     glActiveTexture(GL_TEXTURE0);
 
